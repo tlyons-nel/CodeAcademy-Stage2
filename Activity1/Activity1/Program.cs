0e28ff85f460aed5ed868e0f7e72ac5fdff99879
tlyons-nel <83358682+tlyons-nel@users.noreply.github.com>
2021-07-02 06:02:41 -0600

    Initial Push

---
+++ Activity1/Activity1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllFiles();
+            StreamFile();
+        }
+
+        static void AllFiles()
+        {
+            var lines = File.ReadAllLines("c:\\users\\tlyons\\desktop\\codeacademy\\stage2\\data files\\contacts.csv");
+
+            foreach (var line in lines)
+            {
+                var splits = line.Split(',');
+                Console.WriteLine(splits[1] + " " + splits[2]);
+            }
+        }
+
+        static void StreamFile()
+        {
+            using (StreamReader sr = new StreamReader("c:\\users\\tlyons\\desktop\\codeacademy\\stage2\\data files\\contacts.csv"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var splits = line.Split(',');
+                    Console.WriteLine(splits[1] + " " + splits[2]);
+                }
+            }
+        }
+    }
+}
